533fb003e0e721a4c6f1a7825115c5adbc13076d
Gene Kogan <kogan.gene@gmail.com>
2016-08-16 19:35:41 +0200

    added reverse image search app

---
+++ apps/ReverseImageSearch/src/ofApp.cpp
@@ -0,0 +1,147 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    thumbHeight = ofGetHeight() * 0.25;
+    margin = 5;
+    zoom = 1.25;
+    string file = "lookup.json";
+
+    fullWidth = 0;
+    ofxJSONElement result;
+    bool parsingSuccessful = result.open(file);
+    for (int i=0; i<result.size(); i++) {
+        string path = result[i]["path"].asString();
+        ImageThumb thumb;
+        thumb.image.load(path);
+        thumb.image.resize(thumbHeight * thumb.image.getWidth() / thumb.image.getHeight(), thumbHeight);
+        for (int j=0; j<result[i]["lookup"].size(); j++) {
+            int c = result[i]["lookup"][j].asInt();
+            thumb.closest.push_back(c);
+        }
+        thumbs.push_back(thumb);
+        order.push_back(i);
+        fullWidth += (thumb.image.getWidth() + 5);
+    }
+    random_shuffle(order.begin(), order.end());
+    
+    mx = fullWidth / 2.0;
+    highlighted = -1;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(0);
+
+    int centerX;
+    float x = 10 - mx;
+    float y = thumbHeight * (zoom - 1.0) * 0.5 + 20;
+    for (int i=0; i<thumbs.size(); i++) {
+        if (x > -thumbs[order[i]].image.getWidth() &&
+            x < ofGetWidth()) {
+            thumbs[order[i]].image.draw(x, y);
+        }
+        if (ofGetMouseX() > x && ofGetMouseX() < x+thumbs[order[i]].image.getWidth()) {
+            highlighted = order[i];
+            centerX = x + 0.5 * thumbs[order[i]].image.getWidth();
+        }
+        x += (margin + thumbs[order[i]].image.getWidth());
+    }
+    
+    if (highlighted != -1) {
+        ofPushStyle();
+        ofPushMatrix();
+        ofSetRectMode(OF_RECTMODE_CENTER);
+        ofSetColor(0, 255, 0);
+        ofDrawRectangle(centerX, 0.5 * thumbHeight + y,
+                        zoom * thumbs[highlighted].image.getWidth() + 20,
+                        zoom * thumbs[highlighted].image.getHeight() + 20);
+        ofSetColor(255);
+        thumbs[highlighted].image.draw(centerX, 0.5 * thumbHeight + y,
+                                       zoom * thumbs[highlighted].image.getWidth(),
+                                       zoom * thumbs[highlighted].image.getHeight());
+        ofSetColor(0, 200);
+        ofDrawRectangle(centerX, zoom * thumbHeight + y - 30, 90, 20);
+        ofSetColor(255);
+        ofDrawBitmapString("Query Image", centerX - 43, zoom * thumbHeight + y - 22);
+        ofPopMatrix();
+        ofPopStyle();
+        
+        float x = margin;
+        float y = ofGetHeight() - 2 * (thumbHeight + margin);
+        ofSetColor(255);
+        ofDrawBitmapString("Nearest neighbor images:", x + margin, y - margin);
+        for (int i=0; i<20; i++) {
+            thumbs[thumbs[highlighted].closest[i]].image.draw(x, y);
+            x += (margin + thumbs[thumbs[highlighted].closest[i]].image.getWidth());
+            if (x > (ofGetWidth() - thumbs[thumbs[highlighted].closest[i+1]].image.getWidth()*0.33)) {
+                y += thumbHeight + margin;
+                x = margin;
+            }
+        }
+    }
+    
+    ofSetColor(255);
+    ofDrawBitmapString("Drag mouse to scroll images", 4, 12);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    float newMx = mx + 10 * (ofGetMouseX() - ofGetPreviousMouseX());
+    mx = max(0.0f, min(fullWidth - ofGetWidth(), newMx));
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ apps/ReverseImageSearch/src/ofApp.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxJSON.h"
+
+
+struct ImageThumb {
+public:
+    ofImage image;
+    vector<int> closest;
+    float t;
+};
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+    
+    vector<ImageThumb> thumbs;
+    vector<int> order;
+    
+    float fullWidth;
+    float mx;
+    int highlighted;
+    float thumbHeight;
+    float margin;
+    float zoom;
+};
+
